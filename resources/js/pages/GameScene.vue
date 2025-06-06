ab549e27923cccd51d213310ffe47e3f2781c5e9
AmauryGuillin <amauryg@hotmail.fr>
2025-06-06 22:29:23 +0200

    feat: Bonus page selection

--- resources/js/pages/GameScene.vue
+++ resources/js/pages/GameScene.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { HealthBar } from '@/components/ui/progress-health-bar';
 import { Switch } from '@/components/ui/switch';
@@ -16,6 +17,7 @@ const isGamePaused = ref<boolean>(false);
 const isGameDevModeEnabled = ref<boolean>(true);
 const isEnemiesEnabled = ref<boolean>(true);
 const isPlayerProjectilesEnabled = ref<boolean>(true);
+const isBoostPageOpen = ref<boolean>(false);
 const isHitboxesShown = ref<boolean>(false);
 const projectiles = ref<Projectile[]>([]);
 const enemies = ref<Enemy[]>([]);
@@ -79,12 +81,21 @@ watch(
     },
 );
 
+watch(
+    () => player.value.personalAttributes.score,
+    (score) => {
+        if (score === 5) {
+            isBoostPageOpen.value = true;
+        }
+    },
+);
+
 /**
  * Handle most animations required for the game
  * @beta
  */
 function gameLoop() {
-    if (isGamePaused.value || player.value.personalAttributes.HP <= 0) return;
+    if (isGamePaused.value || isBoostPageOpen.value || player.value.personalAttributes.HP <= 0) return;
 
     handlePlayerMovementAnimation();
 
@@ -256,7 +267,7 @@ function spawnPlayer() {
  * @beta
  */
 function playerStartShooting() {
-    if (!player.value.states.isSpawned || isGamePaused.value) return;
+    if (!player.value.states.isSpawned || isGamePaused.value || isBoostPageOpen.value) return;
     if (!isPlayerProjectilesEnabled.value) return;
     const projectile = markRaw<Projectile>({
         id: crypto.randomUUID(),
@@ -284,6 +295,14 @@ function playerStartShooting() {
 }
 
 /**
+ * Enable the PAUSE state when the bonus selection page is displayed
+ * @beta
+ */
+function HandlePauseStateWhenBonusPageOpen() {
+    isBoostPageOpen.value = false;
+}
+
+/**
  * Handle each player's projectile movement animation
  * @param projectile The player's projectile created in the **playerStartShooting()** function
  * @beta
@@ -307,12 +326,12 @@ function projectileMovement(projectile: Projectile) {
             return;
         }
 
-        if (isGamePaused.value || player.value.personalAttributes.HP > 0) return;
+        if (isGamePaused.value || isBoostPageOpen.value || player.value.personalAttributes.HP > 0) return;
 
         requestAnimationFrame(animateProjectile);
     }
 
-    if (isGamePaused.value || player.value.personalAttributes.HP > 0) return;
+    if (isGamePaused.value || isBoostPageOpen.value || player.value.personalAttributes.HP > 0) return;
 
     requestAnimationFrame(animateProjectile);
 }
@@ -357,6 +376,7 @@ function projectileHit(projectile: Projectile) {
  */
 function spawnEnemy() {
     if (isGamePaused.value) return;
+    if (isBoostPageOpen.value) return;
     if (!isEnemiesEnabled.value) return;
     const enemy = markRaw<Enemy>({
         id: crypto.randomUUID(),
@@ -397,6 +417,7 @@ function spawnEnemy() {
  */
 function spawnPlayerBonus() {
     if (isGamePaused.value) return;
+    if (isBoostPageOpen.value) return;
     const bonus = {
         name: 'Better fire rate',
         bonusType: 'projectile',
@@ -547,7 +568,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div ref="sceneRef" id="scene" class="relative flex h-screen flex-col items-center justify-center bg-gray-900">
+    <div ref="sceneRef" id="scene" class="relative flex h-screen flex-col items-center justify-center bg-gray-700">
         <div v-if="isGameDevModeEnabled" class="absolute top-1 left-[82%] z-50 flex">
             <button class="cursor-pointer rounded-lg border-2 bg-black p-1 font-bold text-white hover:bg-red-600" @click="showHitboxes">
                 Hitboxes
@@ -564,6 +585,40 @@ onUnmounted(() => {
             <button class="cursor-pointer rounded-lg border-2 bg-black p-1 font-bold text-white hover:bg-red-600" @click="playerStartShooting">
                 Shoot!
             </button>
+        </div>
+        <div v-if="isBoostPageOpen" class="z-50 w-[50%] rounded-xl border-2 border-white bg-black p-3 shadow-2xl">
+            <div class="flex items-center justify-center p-3 text-3xl font-bold text-white">
+                <h1>SELECT AN UPGRADE</h1>
+            </div>
+            <div class="flex w-full justify-center gap-5">
+                <Card class="w-1/3">
+                    <CardHeader>
+                        <CardTitle>Movement Speed</CardTitle>
+                        <CardDescription>Increase by 2% your movement speed </CardDescription>
+                    </CardHeader>
+                    <CardFooter>
+                        <Button @click="HandlePauseStateWhenBonusPageOpen" class="w-full cursor-pointer">Select</Button>
+                    </CardFooter>
+                </Card>
+                <Card class="w-1/3">
+                    <CardHeader>
+                        <CardTitle>Projectile Speed</CardTitle>
+                        <CardDescription>Increase by 2% your projectiles speed</CardDescription>
+                    </CardHeader>
+                    <CardFooter>
+                        <Button class="w-full cursor-pointer">Select</Button>
+                    </CardFooter>
+                </Card>
+                <Card class="w-1/3">
+                    <CardHeader>
+                        <CardTitle>Fire Rate</CardTitle>
+                        <CardDescription>Increase by 2% your fire rate</CardDescription>
+                    </CardHeader>
+                    <CardFooter>
+                        <Button class="w-full cursor-pointer">Select</Button>
+                    </CardFooter>
+                </Card>
+            </div>
         </div>
         <Dialog :open="isGamePaused">
             <DialogContent>
