ae83fe6f9c388fc87d8bd5997bfbc5079d72dfc8
AmauryGuillin <amauryg@hotmail.fr>
2025-06-20 22:57:37 +0200

    feat: game font added

--- resources/js/pages/StartMenu.vue
+++ resources/js/pages/StartMenu.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const menuItems = ['Start a new game', 'Settings', 'Quit game'];
+const menuItems = ['Start game', 'Settings', 'Quit game'];
 
 function handleClick(item: string) {
     if (item === menuItems[0]) {
@@ -12,13 +12,7 @@ function handleClick(item: string) {
 </script>
 
 <template>
-    <div class="relative flex max-h-screen min-h-screen w-full flex-col items-center justify-center gap-10">
-        <!-- <img
-            class="absolute top-[50%] left-[18%] z-0 translate-x-[-50%] translate-y-[-50%] overflow-hidden"
-            src="/assets/player/main-menu-player.gif"
-            alt=""
-            srcset=""
-        /> -->
+    <div class="relative flex max-h-screen min-h-screen w-full flex-col items-center justify-center gap-10 font-(family-name:--font-game)">
         <video id="intro-video" class="fixed inset-0 z-0 h-full w-full object-cover" autoplay loop preload="auto">
             <source src="/assets/mainMenu/main-menu.mp4" type="video/mp4" />
             Your browser does not support the video tag.
@@ -26,7 +20,12 @@ function handleClick(item: string) {
         <h1 class="z-50 cursor-default text-9xl font-bold">Light Souls</h1>
         <div class="z-50 flex w-full flex-col items-center justify-center">
             <div class="z-50 flex flex-col items-center justify-center gap-3 text-xl">
-                <h2 v-for="item in menuItems" :key="item" class="cursor-pointer transition-all hover:scale-110" @click="handleClick(item)">
+                <h2
+                    v-for="item in menuItems"
+                    :key="item"
+                    class="cursor-pointer transition-all duration-200 hover:scale-110 hover:font-bold hover:text-orange-400"
+                    @click="handleClick(item)"
+                >
                     {{ item }}
                 </h2>
             </div>
