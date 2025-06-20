7b11edc3b8cf24ff6a38827047ea8e428237a582
AmauryGuillin <amauryg@hotmail.fr>
2025-06-20 22:26:21 +0200

    feat: main menu upgrade

--- resources/js/pages/StartMenu.vue
+++ resources/js/pages/StartMenu.vue
@@ -13,12 +13,16 @@ function handleClick(item: string) {
 
 <template>
     <div class="relative flex max-h-screen min-h-screen w-full flex-col items-center justify-center gap-10">
-        <img
+        <!-- <img
             class="absolute top-[50%] left-[18%] z-0 translate-x-[-50%] translate-y-[-50%] overflow-hidden"
             src="/assets/player/main-menu-player.gif"
             alt=""
             srcset=""
-        />
+        /> -->
+        <video id="intro-video" class="fixed inset-0 z-0 h-full w-full object-cover" autoplay loop preload="auto">
+            <source src="/assets/mainMenu/main-menu.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
         <h1 class="z-50 cursor-default text-9xl font-bold">Light Souls</h1>
         <div class="z-50 flex w-full flex-col items-center justify-center">
             <div class="z-50 flex flex-col items-center justify-center gap-3 text-xl">
