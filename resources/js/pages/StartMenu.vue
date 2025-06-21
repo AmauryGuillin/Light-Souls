0262f99b4d9586c165b68a3f2b7356019d46e351
AmauryGuillin <amauryg@hotmail.fr>
2025-06-22 00:52:14 +0200

    feat: :fire: add some settings options about music volume

--- resources/js/pages/StartMenu.vue
+++ resources/js/pages/StartMenu.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Progress } from '@/components/ui/progress';
+import { Slider } from '@/components/ui/slider';
 import { Howl } from 'howler';
 import { LoaderCircle } from 'lucide-vue-next';
 import { ref } from 'vue';
 
 const menuItems = ['Start game', 'Settings', 'Quit game'];
+const videoRef = ref<HTMLVideoElement | null>(null);
+const settings = ref<boolean>(false);
 const videoLoaded = ref<boolean>(false);
 const videoProgress = ref<number>(0);
+const playerInteracted = ref<boolean>(false);
+const musicON = ref<boolean>(true);
+const soundEffectON = ref<boolean>(true);
+const musicVolume = ref<number>(0.5);
+const soundEffetcsVolume = ref<number>(0.3);
+
+const mainMenuMusic = new Howl({
+    src: ['/assets/music/mainMenu/main-menu.mp3'],
+    volume: musicVolume.value,
+    loop: true,
+});
+
+const soundEffect = new Howl({
+    src: ['/assets/music/mainMenu/selection/Modern1.mp3'],
+    volume: soundEffetcsVolume.value,
+});
 
 function onVideoLoaded() {
     videoLoaded.value = true;
@@ -22,6 +43,22 @@ function onVideoProgress(event: Event) {
     }
 }
 
+function handlePlayerFirstInterfaction() {
+    playerInteracted.value = true;
+    playMenuSelectionSound();
+    videoRef.value?.play();
+}
+
+function handleSettings() {
+    if (!settings.value) {
+        settings.value = true;
+        playMenuSelectionSound();
+        return;
+    }
+    settings.value = false;
+    playMenuSelectionSound();
+}
+
 async function handleClick(item: string) {
     switch (item) {
         case menuItems[0]:
@@ -29,7 +66,7 @@ async function handleClick(item: string) {
             window.location.href = '/LightSouls';
             break;
         case menuItems[1]:
-            console.log('setting');
+            handleSettings();
             break;
         case menuItems[2]:
             window.close();
@@ -41,30 +78,34 @@ async function handleClick(item: string) {
 }
 
 function playMusic() {
-    const sound = new Howl({
-        src: ['/assets/music/mainMenu/main-menu.mp3'],
-        volume: 0.5,
-        loop: true,
-    });
-    sound.play();
+    if (!musicON.value) return;
+    mainMenuMusic.play();
+}
+
+function handleMusicVolume(value: number) {
+    musicVolume.value = value;
+    mainMenuMusic.volume(value);
+}
+
+function handleSoundEffectsVolume(value: number) {
+    soundEffetcsVolume.value = value;
+    soundEffect.volume(value);
 }
 
 function playSelectSound() {
-    const sound = new Howl({
-        src: ['/assets/music/mainMenu/selection/Modern1.mp3'],
-        volume: 0.3,
-    });
-    sound.play();
+    if (!soundEffectON.value) return;
+    soundEffect.play();
 }
 
 function playMenuSelectionSound() {
+    if (!soundEffectON.value) return;
     return new Promise((resolve) => {
-        const sound = new Howl({
+        const soundEffectSelection = new Howl({
             src: ['/assets/music/mainMenu/selection/menu-selection.mp3'],
-            volume: 0.3,
+            volume: soundEffetcsVolume.value,
             onend: resolve,
         });
-        sound.play();
+        soundEffectSelection.play();
     });
 }
 </script>
@@ -72,6 +113,7 @@ function playMenuSelectionSound() {
 <template>
     <div class="relative flex max-h-screen min-h-screen w-full flex-col items-center justify-center gap-10 font-(family-name:--font-game)">
         <video
+            ref="videoRef"
             id="intro-video"
             class="fixed inset-0 z-0 h-full w-full object-cover"
             autoplay
@@ -92,7 +134,7 @@ function playMenuSelectionSound() {
         </div>
         <h1 v-if="videoLoaded" class="z-50 cursor-default text-9xl font-bold drop-shadow-[0_1.2px_1.2px_rgba(252,124,0,1)]">Light Souls</h1>
         <div v-if="videoLoaded" class="z-50 flex w-full flex-col items-center justify-center">
-            <div class="z-50 flex flex-col items-center justify-center gap-3 text-xl">
+            <div v-if="playerInteracted" class="z-50 flex flex-col items-center justify-center gap-3 text-xl">
                 <h2
                     v-for="item in menuItems"
                     :key="item"
@@ -103,6 +145,44 @@ function playMenuSelectionSound() {
                     {{ item }}
                 </h2>
             </div>
+            <div v-else>
+                <span class="animate-pulse cursor-default text-4xl" @click="handlePlayerFirstInterfaction">Click here to continue</span>
+            </div>
+            <Dialog :open="settings && playerInteracted">
+                <DialogContent class="font-game">
+                    <DialogHeader>
+                        <DialogTitle class="text-center">SETTINGS</DialogTitle>
+                        <DialogDescription>
+                            <div class="mt-5 grid w-full grid-cols-3 items-center justify-center gap-5">
+                                <span>Music volume: </span>
+                                <Slider
+                                    class=""
+                                    :max="1"
+                                    :step="0.1"
+                                    :model-value="[musicVolume]"
+                                    @update:modelValue="(val) => handleMusicVolume(val![0])"
+                                />
+                                <span class="text-lg">{{ musicVolume * 10 }}</span>
+                            </div>
+                            <div class="mt-5 grid w-full grid-cols-3 items-center justify-center gap-5">
+                                <span>Sound effects volume: </span>
+                                <Slider
+                                    class=""
+                                    :max="1"
+                                    :step="0.1"
+                                    :model-value="[soundEffetcsVolume]"
+                                    @update:modelValue="(val) => handleSoundEffectsVolume(val![0])"
+                                />
+                                <span class="text-lg">{{ soundEffetcsVolume * 10 }}</span>
+                            </div>
+                        </DialogDescription>
+                    </DialogHeader>
+
+                    <DialogFooter>
+                        <Button @click="handleSettings" class="font-bold"> Save changes </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </div>
     </div>
 </template>
