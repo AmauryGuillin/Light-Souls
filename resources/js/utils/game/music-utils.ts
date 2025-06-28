83f61c30a3a402459f5f110619126794ebc5dfdb
AmauryGuillin <amauryg@hotmail.fr>
2025-06-28 20:08:42 +0200

    refactor: :recycle: comments on music utils functions

--- resources/js/utils/game/music-utils.ts
+++ resources/js/utils/game/music-utils.ts
@@ -1,13 +1,12 @@
 import { Howl } from 'howler';
 
 /**
- * Joue un effet sonore de boule de feu aléatoire.
- * @param soundEffectFireBall Array des chemins vers les sons de boule de feu
- * @param volume Volume du son (0 à 1)
+ * Plays a random fireball sound effect from the provided array.
+ * @param soundEffectFireBall Array of fireball sound file paths.
+ * @param volume Volume for the sound (0 to 1).
  */
 export function playSoundEffectFireBall(soundEffectFireBall: string[], volume: number) {
     const soundEffectIndex = Math.floor(Math.random() * soundEffectFireBall.length);
-
     const soundEffectFireBallSample = new Howl({
         src: [soundEffectFireBall[soundEffectIndex]],
         volume: volume + 0.2,
@@ -16,21 +15,22 @@ export function playSoundEffectFireBall(soundEffectFireBall: string[], volume: n
 }
 
 /**
- * Joue l'effet sonore de power up.
- * @param soundEffectPowerUp Tableau contenant le chemin du son de power up
- * @param volume Volume du son (0 à 1)
+ * Plays the power-up sound effect.
+ * @param soundEffectPowerUp Array containing the power-up sound file path.
+ * @param volume Volume for the sound (0 to 1).
  */
 export function playSoundEffectPowerUp(soundEffectPowerUp: string[], volume: number) {
     const soundEffectPowerUpSample = new Howl({
         src: [soundEffectPowerUp[0]],
         volume: volume,
     });
-
     soundEffectPowerUpSample.play();
 }
 
 /**
- * Joue l'effet sonore de coup sur un ennemi.
+ * Plays the enemy hit sound effect.
+ * @param soundEffectEnemyHit Array containing the enemy hit sound file path.
+ * @param volume Volume for the sound (0 to 1).
  */
 export function playSoundEffectEnemyHit(soundEffectEnemyHit: string[], volume: number) {
     const soundEffectEnemyHitSample = new Howl({
@@ -41,7 +41,9 @@ export function playSoundEffectEnemyHit(soundEffectEnemyHit: string[], volume: n
 }
 
 /**
- * Joue un effet sonore d'attaque d'ennemi aléatoire.
+ * Plays a random enemy attack sound effect from the provided array.
+ * @param soundEffectEnemyAttack Array of enemy attack sound file paths.
+ * @param volume Volume for the sound (0 to 1).
  */
 export function playSoundEffectEnemyAttack(soundEffectEnemyAttack: string[], volume: number) {
     const soundEffectIndex = Math.floor(Math.random() * soundEffectEnemyAttack.length);
@@ -53,7 +55,9 @@ export function playSoundEffectEnemyAttack(soundEffectEnemyAttack: string[], vol
 }
 
 /**
- * Joue l'effet sonore de coup reçu par le joueur.
+ * Plays the player hit sound effect.
+ * @param soundEffectPlayerHit Array containing the player hit sound file path.
+ * @param volume Volume for the sound (0 to 1).
  */
 export function playSoundEffectPlayerHit(soundEffectPlayerHit: string[], volume: number) {
     const soundEffectPlayerHitSample = new Howl({
