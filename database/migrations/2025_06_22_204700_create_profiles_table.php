095c4696447d1d15ae07252fbda53374c930833f
AmauryGuillin <amauryg@hotmail.fr>
2025-07-02 19:57:27 +0200

    refactor: initialise profile with 0 instead of nulls

--- database/migrations/2025_06_22_204700_create_profiles_table.php
+++ database/migrations/2025_06_22_204700_create_profiles_table.php
@@ -14,16 +14,16 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->integer('max_survival_time')->nullable();
-            $table->integer('last_game_enemies_killed')->nullable();
-            $table->integer('enemies_killed_best')->nullable();
-            $table->integer('total_enemies_killed')->nullable();
-            $table->integer('total_game_played')->nullable();
-            $table->integer('total_hour_played')->nullable();
-            $table->integer('death_count')->nullable();
-            $table->integer('higher_level')->nullable();
-            $table->integer('total_fireball_shot')->nullable();
-            $table->integer('total_player_dps')->nullable();
+            $table->integer('max_survival_time')->default(0);
+            $table->integer('last_game_enemies_killed')->default(0);
+            $table->integer('enemies_killed_best')->default(0);
+            $table->integer('total_enemies_killed')->default(0);
+            $table->integer('total_game_played')->default(0);
+            $table->integer('total_hour_played')->default(0);
+            $table->integer('death_count')->default(0);
+            $table->integer('higher_level')->default(1);
+            $table->integer('total_fireball_shot')->default(0);
+            $table->integer('total_player_dps')->default(0);
             $table->timestamps();
         });
     }
