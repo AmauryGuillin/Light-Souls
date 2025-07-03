74092624c04e45d756c74b278bb86b22e2b615ed
AmauryGuillin <amauryg@hotmail.fr>
2025-07-03 20:12:18 +0200

    fix: :zap: player start level 1...

--- resources/js/pages/GameScene.vue
+++ resources/js/pages/GameScene.vue
@@ -220,7 +220,7 @@ function spawnPlayer() {
     player.value.position.Y = 50;
     player.value.name = 'Player';
     player.value.states.isSpawned = true;
-    player.value.personalAttributes.level = 5;
+    player.value.personalAttributes.level = 1;
     gameStartTime.value = Date.now();
 }
 
